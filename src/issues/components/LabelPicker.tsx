1ab3d99dcc65b2e55d7685c93d1709c160ad7760
Roger Reyes <kadoshrr2019@gmail.com>
2023-06-20 23:23:42 -0500

    Installed Axios, Consuming github API Section Labels in Label Picker Component, Maked directory interfaces and Created label interface

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+
+
+const getLabels = async():Promise<Label[]> => {
+    const { data } = await githubApi.get<Label[]>('/labels')
+    return data;
+}
+
 export const LabelPicker = () => {
+    const labelsQuery = useQuery(
+        ['labels'],
+        getLabels,
+    );
+
     return (
       <div>
           <span 
