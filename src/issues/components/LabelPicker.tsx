bc42a72d0438c0ce2c9c795056549f4922cc9f8d
Roger Reyes <kadoshrr2019@gmail.com>
2023-06-21 00:36:50 -0500

    Added Selected Labels State with onChange, Created label active Styles, Added Ternary Operator condition in Label Picker

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,24 +1,35 @@
-import { LoadingIcon } from "../../shared/components/LoadingIcon";
-import { useLabels } from "../hooks/useLabels"
+import { FC } from 'react';
+import { useLabels } from '../hooks/useLabels';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
-export const LabelPicker = () => {
-    const labelsQuery = useLabels();
-    if (labelsQuery.isLoading){
-        return (<LoadingIcon />)
-    }
-        
-    return (
-      <div>
-        {
-            labelsQuery.data?.map(label =>(
-            <span key={label.id}
-              className="badge rounded-pill m-1 label-picker"
-              style={{ border: `1px solid #${(label.color)}`, color: `#${label.color}`}}
-            >
-              {label.name}
-            </span>
-            ))
-        } 
-      </div>
-    )
-  }
+
+interface Props {
+  selectedLabels: string[];
+  onChange: ( labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange }) => {
+
+  const labelsQuery = useLabels();
+
+  if ( labelsQuery.isLoading ) //! por qué isLoading y no isFetching
+    return ( <LoadingIcon /> );
+
+
+  return (
+    <>
+      {
+        labelsQuery.data?.map( label => (
+          <span 
+              key={ label.id }
+              className={`badge rounded-pill m-1 label-picker ${ selectedLabels.includes(label.name) ? 'label-active': '' }`}
+              style={{ border: `1px solid #${ label.color }`, color: `#${ label.color }` }}
+              onClick={ () => onChange( label.name ) }
+          >
+              { label.name }
+          </span>
+        ))
+      }
+    </>
+  )
+}
